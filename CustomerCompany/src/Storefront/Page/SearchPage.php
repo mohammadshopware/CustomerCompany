a333d953b527c222a003644103645151ebefc5ac
shopware <shopware@server.com-new>
2025-03-14 07:06:34 +0530

    Improve frontend page

--- CustomerCompany/src/Storefront/Page/SearchPage.php
+++ CustomerCompany/src/Storefront/Page/SearchPage.php
@@ -8,7 +8,7 @@ class SearchPage extends Page
 {
     protected $customersResult;
 
-    protected $responseData;
+    protected $responseData = [];
 
     public function getCustomersResult() : EntityCollection
     {
